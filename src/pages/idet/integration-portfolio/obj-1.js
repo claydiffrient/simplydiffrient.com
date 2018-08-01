2597c69d207186fe20c590a45ca101e19863991a
Clay Diffrient <clay.diffrient@gmail.com>
2018-08-01 16:54:31 -0600

    Add objective 1

--- src/pages/idet/integration-portfolio/obj-1.js
+++ src/pages/idet/integration-portfolio/obj-1.js
@@ -16,6 +16,35 @@ const ObjectiveOne = () => (
       Objective 1: Identify and know how to locate various kinds of technology
       resources and relate them to the teaching process.
     </h3>
+    <p>
+      I am capable of showing how to relate various technologies to teaching.
+    </p>
+    <h3>Artifact</h3>
+    <iframe
+      title="Technology Integration Matrix"
+      css={{
+        width: "100%"
+      }}
+      src="https://clay.app.box.com/embed/s/xcikhugyal2th42os92hnoag9h8swe8n"
+      width="500"
+      height="400"
+      frameborder="0"
+      allowfullscreen
+      webkitallowfullscreen
+      msallowfullscreen
+    />
+    <h4>Reflection</h4>
+    <p>
+      Completing the matrix shown in the artifact section gave me a lot of
+      insight into how various technologies get adopted and used for teaching
+      and learning. It shows that I am able to identify and relate learning to
+      technology use.
+    </p>
+    <h4>Goal</h4>
+    <p>
+      I plan to continue to critically evaluate the use of technology with
+      education.
+    </p>
   </Layout>
 );
 
