aa924f128493bd820459d8cca6706952f97bd6d8
Clay Diffrient <clay.diffrient@gmail.com>
2018-08-08 23:26:50 -0600

    Move to using nginx to host

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,17 @@
-FROM node:10
-
-EXPOSE 8080
-
+FROM node:10 as build
 ENV APP_HOME /usr/src/app/
-
 RUN mkdir -p $APP_HOME
 WORKDIR $APP_HOME
-
 COPY package.json           ${APP_HOME}
 COPY package-lock.json      ${APP_HOME}
-
 RUN npm install
-
-RUN wget https://github.com/Yelp/dumb-init/releases/download/v1.2.1/dumb-init_1.2.1_amd64.deb
-RUN dpkg -i dumb-init_*.deb
-
 COPY . ${APP_HOME}
-
-COPY CHECKS /app/CHECKS
-
 RUN npm run build
 
-
-CMD ["dumb-init", "npm", "start"]
+FROM alpine:latest
+EXPOSE 8080
+COPY --from=build /usr/src/app/public /app/public
+COPY CHECKS /app/CHECKS
+RUN apk add --no-cache nginx
+ADD nginx.conf /
+ENTRYPOINT ["nginx", "-c",  "/nginx.conf"]
--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-/**
- * Static HTTP Server
- *
- * Create a static file server instance to serve files
- * and folder in the './public' folder
- */
+const http2 = require("http2");
+const fs = require("fs");
+const path = require("path");
+const mime = require("mime-types");
 
-// modules
-var static = require("node-static"),
-  port = 8080,
-  http = require("http");
+const {
+  HTTP2_HEADER_PATH,
+  HTTP2_HEADER_METHOD,
+  HTTP_STATUS_NOT_FOUND,
+  HTTP_STATUS_INTERNAL_SERVER_ERROR
+} = http2.constants;
 
-// config
-var file = new static.Server("./public", {
-  cache: 31536000,
-  gzip: true
-});
+// /**
+//  * Static HTTP Server
+//  *
+//  * Create a static file server instance to serve files
+//  * and folder in the './public' folder
+//  */
 
-// serve
-http
-  .createServer(function(request, response) {
-    request
-      .addListener("end", function() {
-        file.serve(request, response);
-      })
-      .resume();
-  })
-  .listen(port);
+// // modules
+// var static = require("node-static"),
+//   port = 8080,
+//   http = require("http");
+
+// // config
+// var file = new static.Server("./public", {
+//   cache: 31536000,
+//   gzip: true
+// });
+
+// // serve
+// http
+//   .createServer(function(request, response) {
+//     request
+//       .addListener("end", function() {
+//         file.serve(request, response);
+//       })
+//       .resume();
+//   })
+//   .listen(port);
