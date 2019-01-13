ab27405ad7fc5978d89f1375ca13b94dc034df0f
Clay Diffrient <clay.diffrient@gmail.com>
2019-01-12 19:21:21 -0700

    Add babel-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,9 @@ module.exports = {
     "plugin:security/recommended",
     "prettier/react"
   ],
+  parser: "babel-eslint",
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2019,
     sourceType: "module",
     ecmaFeatures: {
       jsx: true
