21597ddfaff22e43dc74cd216c941b3050f1badc
Clay Diffrient <clay.diffrient@gmail.com>
2019-01-14 00:00:15 -0700

    Add initial work on legislation tracking blog

--- src/pages/idet.js
+++ src/pages/idet.js
@@ -4,6 +4,11 @@ import Helmet from "react-helmet";
 import { defaultBtn } from "../utils/variables";
 import { Link } from "gatsby";
 
+const navButtonStyle = {
+  width: "100%",
+  ...defaultBtn
+};
+
 const IDET = () => (
   <Layout>
     <Helmet>
@@ -16,8 +21,13 @@ const IDET = () => (
       Education.
       <ul css={{ marginTop: "15px", listStyle: "none" }}>
         <li css={{ listStyle: "unstyled" }}>
-          <Link to="/idet/integration-portfolio" css={defaultBtn}>
+          <Link to="/idet/integration-portfolio" css={navButtonStyle}>
             Integration of Educational Technology - Portfolio
+          </Link>
+        </li>
+        <li css={{ listStyle: "unstyled" }}>
+          <Link to="/idet/utah-legislation" css={navButtonStyle}>
+            Leadership in IDET - Utah Legislature Tracking
           </Link>
         </li>
       </ul>
---
+++ src/pages/idet/utah-legislation/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Layout from "../../../components/layout";
+import Helmet from "react-helmet";
+
+const UtahLegislation = () => (
+  <Layout>
+    <Helmet>
+      <title>Simply Diffrient - Utah Legislation Tracking</title>
+    </Helmet>
+    <h2>Leadership in IDET - Utah Legislature Tracking</h2>
+    <p>
+      This blog follows my learnings about a piece of legislature brought
+      forward during the 2019 general session of the Utah Legislature. It is a
+      project for my Leadership in IDET course (ED PS 6445) being completed in
+      Spring 2019 at the University of Utah.
+    </p>
+  </Layout>
+);
+
+export default UtahLegislation;
