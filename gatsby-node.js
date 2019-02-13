49e4a2cf80fb12afedd836677a959fc0c4cd9735
Clay Diffrient <clay.diffrient@gmail.com>
2019-02-13 16:37:45 -0700

    Add individual view of entries

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,51 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
+const { createFilePath } = require("gatsby-source-filesystem");
+const path = require("path");
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` });
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug
+    });
+  }
+};
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              category
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      if (node.frontmatter.category === "utah-legislature-project") {
+        createPage({
+          path: `idet/utah-legislation${node.fields.slug}`,
+          component: path.resolve(`./src/templates/blog-post.js`),
+          context: {
+            // Data passed to context is available
+            // in page queries as GraphQL variables.
+            slug: node.fields.slug
+          }
+        });
+      }
+    });
+  });
+};
