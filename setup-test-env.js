3cd7f9e203b59939c85833e7df3a4d0e486ad09c
Clay Diffrient <clay.diffrient@gmail.com>
2021-03-22 09:15:30 -0600

    Remove unnecessary cleanup line

--- setup-test-env.js
+++ setup-test-env.js
@@ -1,8 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
 
-// this is basically: afterEach(cleanup)
-import "@testing-library/react/cleanup-after-each";
-
 import { toMatchImageSnapshot } from "jest-image-snapshot";
 
 expect.extend({ toMatchImageSnapshot });
