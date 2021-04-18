7ffa317d10194ba780ba3fdf82a4ea4d31d9f519
Clay Diffrient <clay.diffrient@gmail.com>
2021-04-18 13:06:39 -0600

    Make jest work with ts files

--- jest-preprocess.js
+++ jest-preprocess.js
@@ -1,5 +1,10 @@
 const babelOptions = {
-  presets: ["@babel/react", "@babel/env", "babel-preset-gatsby"],
+  presets: [
+    "@babel/react",
+    "@babel/env",
+    "babel-preset-gatsby",
+    "@babel/preset-typescript",
+  ],
   plugins: [
     "@babel/plugin-proposal-optional-chaining",
     "@babel/plugin-transform-async-to-generator",
